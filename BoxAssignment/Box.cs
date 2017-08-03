7f10c3c7ee03e25a861cf6b95646291b88bfc98f
jaymoddo <jibril808@gmail.com>
2017-08-03 18:27:51 -0500

    first commit

---
+++ BoxAssignment/Box.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace BoxAssignment
+{
+    public class Box
+    {
+
+        public double cost { set; get; }
+        public double height { set; get; }
+        public double length { set; get; }
+        public double width { set; get; }
+
+
+                
+        }
+    }
+
---
+++ BoxAssignment/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace BoxAssignment
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Box object
+            Box b1 = new Box();
+
+           //Request for user input
+            Console.WriteLine("Enter the cost of material per square inch: ");
+            double matCost = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the height in inches: ");
+            double h = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the length in inches: ");
+            double l = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the width in inches: ");
+            double w = Convert.ToDouble(Console.ReadLine());
+
+            //Calculation for volume and total surface area
+            double v = h * l * w;
+            double a1 = h * w;
+            double a2 = h * l;
+            double a3 = w * l;
+            double surfaceArea = 2*(a1) + 2*(a2) + 2*(a3);
+
+              //Display results
+               Console.WriteLine("The volume of this box is " + "{0}" + " cubic inches.", v);
+               Console.WriteLine("The total surface area " + "{0}" + " sq. inches.", surfaceArea);
+               Console.WriteLine("This box costs " + "${0}" + " per sq. inch.", matCost);
+        }
+    }
+}
+
